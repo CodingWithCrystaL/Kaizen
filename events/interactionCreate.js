5718b61c0f9e886d00c19e5fdb1fd4e620910cc1
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-05-09 03:50:37 +0530

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (err) {
+      console.error(err);
+      await interaction.reply({ content: '❌ Error executing command.', ephemeral: true });
+    }
+  }
+};
