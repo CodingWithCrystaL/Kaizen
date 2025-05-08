c16ca367f823e2762f41db734833e7cc37b095d4
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-05-09 04:27:11 +0530

    Update lock.js

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -3,15 +3,17 @@ const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('disc
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('lock')
-    .setDescription('🔒 Lock the current channel (prevent messaging)')
+    .setDescription('🔒 Lock the current channel')
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
 
   async execute(interaction) {
-    await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SendMessages: false });
+    await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+      SendMessages: false
+    });
 
     const embed = new EmbedBuilder()
       .setTitle('🔒 Channel Locked')
-      .setDescription('This channel is now in lockdown mode.')
+      .setDescription('This channel is now locked.')
       .setColor('#ffffff')
       .setFooter({ text: `${interaction.guild.name} | 🌙Made By Kai` })
       .setTimestamp();
