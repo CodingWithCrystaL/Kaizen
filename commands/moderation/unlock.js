c9759d41cc767c56b89b7e706815e7d23787ddb4
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-05-09 03:46:04 +0530

    Create unlock.js

---
+++ commands/moderation/unlock.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('unlock')
+    .setDescription('🔓 Unlock the current channel (allow messaging)')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
+  async execute(interaction) {
+    await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SendMessages: true });
+
+    const embed = new EmbedBuilder()
+      .setTitle('🔓 Channel Unlocked')
+      .setDescription('This channel is now open for messaging.')
+      .setColor('#ffffff')
+      .setFooter({ text: `${interaction.guild.name} | 🌙Made By Kai` })
+      .setTimestamp();
+
+    interaction.reply({ embeds: [embed] });
+  }
+};
