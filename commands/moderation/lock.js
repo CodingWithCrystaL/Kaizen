353282f23d87f7537f24e2d8815eb3b7f6ce7892
CrystaL <80718023+CodingWithCrystaL@users.noreply.github.com>
2025-05-11 05:10:38 +0530

    Update lock.js

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -7,15 +7,18 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
 
   async execute(interaction) {
-    await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+    const isSlash = !!interaction.commandName;
+    const channel = interaction.channel;
+
+    await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
       SendMessages: false
     });
 
     const embed = new EmbedBuilder()
       .setTitle('🔒 Channel Locked')
-      .setDescription('This channel is now locked.')
+      .setDescription(`> This channel has been **locked** for everyone.`)
       .setColor('#ffffff')
-      .setFooter({ text: `${interaction.guild.name} | 🌙Made By Kai` })
+      .setFooter({ text: `${interaction.guild.name}` })
       .setTimestamp();
 
     interaction.reply({ embeds: [embed] });
